a590d5736b9be16cc9f9791942387b8ed66993a9
Ethan Sherbondy <sherbondye@gmail.com>
2012-07-09 23:09:32 -0700

    minimum viable demo. Now to hook up some iboutlets...

--- nibswap/AppDelegate.m
+++ nibswap/AppDelegate.m
@@ -7,6 +7,10 @@
 //
 
 #import "AppDelegate.h"
+#import <SSZipArchive.h>
+
+// replace this with the base url where you stored example.nib.zip
+static NSString *kRemoteNibBaseURL = @"http://localhost/~ethanis/";
 
 @implementation AppDelegate
 
@@ -15,37 +19,66 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    return [self downloadBundle];
+}
+
+- (BOOL)downloadBundle
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    
+    NSString *bundleFilename = @"example.bundle";
+    NSString *zipFilename = [bundleFilename stringByAppendingString:@".zip"];
+    NSURL *url = [NSURL URLWithString:[kRemoteNibBaseURL stringByAppendingString:zipFilename]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLResponse *response = nil;
+    NSError *error = nil;
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+    NSLog(@"%d", [httpResponse statusCode]);
+        
+    if ([httpResponse statusCode] == 404) // bundle will be deleted and the default interface will be used ...
+    {
+        NSString *path = [documentsDirectory stringByAppendingPathComponent:bundleFilename];
+        [[NSFileManager defaultManager] removeItemAtPath:path error:nil];
+        return NO;
+    }
+    else if (error)
+    {
+        NSLog(@"%@", error);
+    }
+    
+    NSString *zipFile = [documentsDirectory stringByAppendingString:zipFilename];
+    BOOL didWriteData = [data writeToFile:zipFile atomically:YES];
+    if (didWriteData)
+    {
+        BOOL success = [SSZipArchive unzipFileAtPath:zipFile toDestination:documentsDirectory];
+        if (!success)
+        {
+            NSLog(@"failed to unzip file.");
+        } else {
+            NSLog(@"Success!");
+        }
+    }
+    
+    NSString *file = [documentsDirectory stringByAppendingPathComponent:bundleFilename];
+    NSBundle *bundle = [NSBundle bundleWithPath:file];
+    if (!bundle)
+    {
+        NSLog(@"no bundle found.");
+    }
+    
+    UIViewController *viewController = [[UIViewController alloc] init];
+    NSArray *nibs = [bundle loadNibNamed:@"ExampleView" owner:viewController options:nil];
+    // use options to hook up iboutlets
+    UIView *nibView = [nibs objectAtIndex:0];
+
+    self.window.rootViewController = viewController;
+    viewController.view = nibView;
+    
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
