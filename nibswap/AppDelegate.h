bd476312fbbc5af8a73526fccf8837b49de9f7d6
Ethan Sherbondy <sherbondye@gmail.com>
2012-07-09 21:58:11 -0700

    Initial Commit

---
+++ nibswap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nibswap
+//
+//  Created by Ethan Sherbondy on 7/9/12.
+//  Copyright (c) 2012 MIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ nibswap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  nibswap
+//
+//  Created by Ethan Sherbondy on 7/9/12.
+//  Copyright (c) 2012 MIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
